0cd80ee14e3c86e521af4ab61e363a76a18651cb
Aditya <adityalalwani05@gmail.com>
2021-06-29 15:31:30 +0530

    More Components into Separate Files

---
+++ 3.js
@@ -0,0 +1,7 @@
+//More Components into separate files
+import React from "react";
+import ReactDOM from "react-dom";
+
+import MyInfo from "./components/MyInfo";
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
