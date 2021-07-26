cdb2f565efee21ece25e91c715e3966bfcbe09aa
Aditya <adityalalwani05@gmail.com>
2021-07-26 12:18:55 +0530

    React Conditional Render

---
+++ 18/Conditional.js
@@ -0,0 +1,8 @@
+//React Conditional Render
+import React from "react";
+
+function Conditional(props) {
+	return <h1>Some cool stuff about conditional rendering</h1>;
+}
+
+export default Conditional;
